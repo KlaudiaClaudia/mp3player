2f5aa56ae7d7913c6ac0e0cd5c81628f20a8a546
KlaudiaClaudia <claudiadabpriv@gmail.com>
2021-05-14 12:18:26 +0200

    Connected with SceneBuilder

--- src/main/java/pl/klaudia/mp3player/main/Main.java
+++ src/main/java/pl/klaudia/mp3player/main/Main.java
@@ -1,6 +1,9 @@
 package pl.klaudia.mp3player.main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,7 +14,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Mp3 Player");
+        Pane mainPane = FXMLLoader.load(getClass().getResource("/fxml/mainPane.fxml"));
+        Scene scene = new Scene(mainPane);
+        stage.setScene(scene);
+        stage.setTitle("MP3 Player");
         stage.show();
+
     }
 }
